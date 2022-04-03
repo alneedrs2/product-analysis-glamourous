fd67fb70577317a7a26292d9113b3737730b62fc
Alneed Roy <alneedrs2@gmail.com>
2022-04-04 00:51:34 +0600

    tailwind css linked

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h6>hii i am here to know you</h6>
+        </div>
+    );
+};
+
+export default Reviews;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
